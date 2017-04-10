86a474600448a42701dfbe35dc5cf0a375b996e4
code2get <sunriseview@outlook.com>
2017-04-10 15:01:50 +0000

    + getInitialState() and setState() used to change name to this.refs.name.value, that is provided by user

--- public/app.jsx
+++ public/app.jsx
@@ -6,14 +6,28 @@ var Greeter = React.createClass({
     };
   },
   
+  getInitialState: function () {
+    return {
+      name: this.props.name
+    };
+  },
+  
   onButtonClick: function (e) {
     e.preventDefault();
-    var name = this.refs.name.value;
-    alert(name);
+    
+    var nameRef = this.refs.name;
+    var name = nameRef.value;
+    nameRef.value = '';
+    
+    if (typeof name === 'string' && name.length > 0) {
+      this.setState({
+        name: name
+      });
+    }
   },
   
   render: function () {
-    var name = this.props.name;
+    var name = this.state.name;
     var message = this.props.message;
     
     return (
