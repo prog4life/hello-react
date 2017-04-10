bd250369a1658e68e76b12e7f85cbc60d3b7243c
code2get <sunriseview@outlook.com>
2017-04-10 13:46:56 +0000

    Another one message prop passed and it's default value added to getDefaultProp method

--- public/app.jsx
+++ public/app.jsx
@@ -1,22 +1,26 @@
 var Greeter = React.createClass({
   getDefaultProps: function () {
     return {
-      name: "React"
+      name: "React",
+      message: "This is the default message!"
     };
   },
   render: function () {
     var name = this.props.name;
+    var message = this.props.message;
     
     return (
       <div>
         <h1>Hello {name}!</h1>
-        <p>This is from a Component!</p>
+        <p>{message + '!!'}</p>
       </div>
     );
   }  
 }); 
 
+var firstName = "JC";
+
 ReactDOM.render(
-  <Greeter name="JC" />, 
+  <Greeter name={firstName} message="Message from passed prop!" />, 
   document.getElementById('app')
 );
