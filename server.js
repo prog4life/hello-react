ca7c88982d8aff36ceb26fe0e85e1c840d72fa2a
prog4life <sunriseview@outlook.com>
2017-04-12 02:31:36 +0300

    Minor changes done in server: IP and PORT as vars, 'localhost' without 'http://' as default

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 3000, process.env.IP || 'http://localhost', () => {
-  console.log('Server is up on: ', process.env.IP, ':', process.env.PORT);
+var IP = process.env.IP || 'localhost';
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, IP, () => {
+  console.log('Server is up on: ', IP, ':', PORT);
 });
