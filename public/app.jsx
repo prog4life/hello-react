b88d3206256a7dd3a4151305f2d04bde52b48ea0
prog4life <sunriseview@outlook.com>
2017-04-19 17:47:51 +0300

    + .eslintrc.json and few corrections in proj files

--- public/app.jsx
+++ public/app.jsx
@@ -1,9 +1,9 @@
 var GreeterMessage = React.createClass({
-  
+
   render: function () {
     var name = this.props.name;
     var message = this.props.message;
-    
+
     return (
       <div>
         <h1>Hello {name}!</h1>
@@ -14,28 +14,28 @@ var GreeterMessage = React.createClass({
 });
 
 var GreeterForm = React.createClass({
-  
+
   onFormSubmit: function (e) {
     e.preventDefault();
-    
+
     var updates = {};
-    
+
     var name = this.refs.name.value;
     var message = this.refs.message.value;
-    
+
     if (name.length > 0) {
-      this.refs.name.value = '';
+      this.refs.name.value = "";
       updates.name = name;
     }
-    
+
     if (message.length > 0) {
-      this.refs.message.value = '';
+      this.refs.message.value = "";
       updates.message = message;
     }
-    
+
     this.props.onNewData(updates);
   },
-  
+
   render: function () {
     return (
       <form onSubmit={this.onFormSubmit}>
@@ -48,49 +48,49 @@ var GreeterForm = React.createClass({
         <div>
           <button>Submit</button>
         </div>
-        
+
       </form>
     );
   }
 });
 
 var Greeter = React.createClass({
-  
+
   getDefaultProps: function () {
     return {
       name: "React",
       message: "This is the default message!"
     };
   },
-  
+
   getInitialState: function () {
     return {
       name: this.props.name,
       message: this.props.message
     };
   },
-  
+
   handleNewData: function (updates) {
     this.setState(updates);
   },
-  
+
   render: function () {
     var name = this.state.name;
     var message = this.state.message;
-    
+
     return (
       <div>
         <GreeterMessage name={name} message={message} />
-        
+
         <GreeterForm onNewData={this.handleNewData} />
       </div>
     );
-  }  
-}); 
+  }
+});
 
 var firstName = "JC";
 
 ReactDOM.render(
-  <Greeter name={firstName} />, 
-  document.getElementById('app')
+  <Greeter name={firstName} />,
+  document.getElementById("app")
 );
